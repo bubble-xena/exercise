9e44ce72a7d56408a9736b70eae4f0cbd906f168
xiaosiqi <1843345703@qq.com>
2020-03-18 15:50:13 +0800

    no message

---
+++ cmake-build-debug/CMakeFiles/exercise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise.dir/main.c.o"
+  "exercise"
+  "exercise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
